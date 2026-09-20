1b919cad6ba182c6955a9cded656bc664c4ebc96
Dianaayunurazizah <dianaayunurazizah646@gmail.com>
2026-09-20 21:52:41 +0700

    commit 1

---
+++ src/pbo/Latihan.java
@@ -0,0 +1,33 @@
+package pbo;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public class Latihan {
+    String nim;
+    String nama;
+    String jurusan;
+
+    public Latihan(String nim, String nama, String jurusan) {
+        this.nim = nim;
+        this.nama = nama;
+        this.jurusan = jurusan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+}
---
+++ src/pbo/Pbo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbo;
+
+/**
+ *
+ * @author Acer
+ */
+public class Pbo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Latihan lth = new Latihan("123", "ANNA", "AKUTANSI");
+        lth.setNama("Diana");
+        System.out.println("NAMA :" + lth.getNama());
+    }
+    
+}
