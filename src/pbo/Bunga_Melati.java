7aae284d6c5a04e658893af42c236c7cfd12f0f1
Dianaayunurazizah <dianaayunurazizah646@gmail.com>
2026-09-20 23:27:04 +0700

    diana add class bunga melati

---
+++ src/pbo/Bunga_Melati.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo;
+
+/**
+ *
+ * @author Acer
+ */
+public class Bunga_Melati {
+    String negaraAsal;
+    String makna;
+    String musimMekar;
+    
+    public Bunga_Melati(String negaraAsal,String makna,String musimMekar ){
+        this.negaraAsal=negaraAsal;
+        this.makna=makna;
+        this.musimMekar=musimMekar;
+    }
+    
+    public void setnegaraAsal (String negaraAsal){
+        this.negaraAsal = negaraAsal;
+    }
+    public void getnegaraAsal (String negaraAsal){
+        this.negaraAsal = negaraAsal;
+    }
+    public void setmakna (String makna){
+        this.makna = makna;
+    }
+    public void getmakna (String makna){
+        this.makna = makna;
+    }
+    public void setmusimMekar (String musimMekar){
+        this.musimMekar = musimMekar;
+    }
+    public void getmusimMekar (String musimMekar){
+        this.musimMekar = musimMekar;
+    }
+    
+}
