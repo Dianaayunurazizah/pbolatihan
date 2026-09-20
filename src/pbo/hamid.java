e5f9195eb3b7e211f00742c9e41268ae8fa06e2a
rohmanhamit <hamid211004@gmail.com>
2026-09-20 22:29:46 +0700

    add class hamid

---
+++ src/pbo/hamid.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo;
+
+/**
+ *
+ * @author ASUS
+ */
+public class hamid {
+    String nim;
+    String nama;
+    String prodi;
+    String alamat;
+
+    public hamid(String nim, String nama, String prodi, String alamat) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.alamat = alamat;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+    
+    
+}
